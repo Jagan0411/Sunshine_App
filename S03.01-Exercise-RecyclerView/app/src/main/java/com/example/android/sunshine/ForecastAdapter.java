9c29000067f2008a06471b255298f22e0ffdc39b
CLJ <cl.jagan@gmail.com>
2017-06-26 00:41:35 -0700

    S03.01-Ex-RecyclerView

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,70 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by harish on 26-06-2017.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String mWeatherData[];
+
+    public ForecastAdapter()
+    {
+
+    }
+
+    @Override
+    public ForecastAdapter.ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int listItemId = R.layout.forecast_list_item;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        boolean attachParent = false;
+
+        View view = layoutInflater.inflate(listItemId,parent,attachParent);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapter.ForecastAdapterViewHolder holder, int position) {
+
+        String weather_now = mWeatherData[position];
+        holder.mWeatherTextView.setText(weather_now);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(null == mWeatherData)
+        {
+            return 0;
+        }
+            return mWeatherData.length;
+    }
+
+    void setWeatherData(String weatherData[])
+    {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view)
+        {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+
+    }
+
+}
